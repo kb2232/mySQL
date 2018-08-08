b82d5d66aaa24d80deead91066e1636f9c769948
kunle babatunde <kb2232@nyu.edu>
2018-08-08 14:31:41 -0400

    final submission

--- dev/sequel1.js
+++ dev/sequel1.js
@@ -8,6 +8,9 @@ require('dotenv').config();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+app.use(function(req, res, next) {
+	next();
+});
 
 /////////CONNECTON///////////////////////////////////////
 var con = sql.createConnection({
@@ -33,20 +36,43 @@ con.connect(function(err)
 	// user1.createTable(con);
   // user1.insertValues(con);
 	// user1.printALL(con);
+	// user2.createTable(con);
+	// user2.insertValues(con);
+	// user2.printALL(con);
 });
 /////////////////////////////SERVER/////////////////////////////////////////////
 //server
-app.get('/',(req,res)=>{
-	res.render("home")
+app.get('/',(req,res,next)=>
+{
+	var c = `SELECT COUNT(*) AS 'count' FROM ${tableName}`;
+	var q = `SELECT prod_name, department_name, price, stock_quantity
+	FROM ${tableName}
+	ORDER BY item_id
+	ASC`;
+	con.query(q,(err,result)=>
+	{
+		if(err) throw err;
+		var tes = " ";
+		for(let x = result.length-1; x<result.length; x++)
+		{
+			let res = result[x].prod_name+"--"+result[x].department_name+"--"+result[x].price+"--"+result[x].stock_quantity+"\n|\n";
+			tes +=res;
+		}
+		res.render("home",
+		{ 
+			data: tes,
+			len: result.length
+		});
+	});
 });
+
 app.get("/joinus",(req,res)=>{
 		//total items we have in database
-		var q = "SELECT COUNT(*) AS 'count' FROM test";
+		var q = `SELECT COUNT(*) AS 'count' FROM ${tableName}`;
 		con.query(q,(err,result)=>{
 			if (err) throw err;
 			var count = result[0].count;
 			res.render("joinus",{ count: count } );
-			//res.send("We have "+count+" items in our database");
 		});
 });
 app.post('/register',function(req,res){
@@ -56,10 +82,66 @@ app.post('/register',function(req,res){
 		price : req.body.price,
 		stock_quantity : req.body.quantity
 	}
-	con.query('INSERT INTO test SET ?',newItem,(err,result)=>{
+	con.query(`INSERT INTO ${tableName} SET ?`,newItem,(err,result)=>{
 		if (err) throw err;
 		res.redirect("/");
 	});
+});
+//BUY FROM US 
+app.get('/buyfromus',(req,res)=>
+{
+	let q = `SELECT item_id, stock_quantity
+	FROM ${tableName}`;
+	let output = "\n";
+	con.query(q,(err,result)=>{
+		if(err) throw err;
+		for(let i = 0; i<result.length; i++)
+		{
+			output += result[i].item_id +
+								"------"+result[i].stock_quantity +
+								" ||||||||| ";
+		}
+		res.render("buyfromus",{
+			data: output
+		});
+	});
+});
+
+app.post('/buynow',(req,res,next)=>
+{
+	var newItem = {
+		item_id : req.body.item_id,
+		stock_quantity : req.body.quantity
+	}
+	con.query(`SELECT * FROM ${tableName}
+	WHERE item_id=${newItem.item_id}`,(err,result)=>
+	{
+		var response="...";
+		if (err) throw err;
+		if(result[0]===undefined) {
+			response="Item Id does not exist!!!";
+			console.log(response);
+		}
+		else if(result[0].stock_quantity < newItem.stock_quantity) {
+			response="Insufficient amount of product";
+			console.log(response);
+		}
+		else {	
+			response="Item-"+result[0].item_id+":\nYou purchased "+req.body.quantity+" "+result[0].prod_name+" from the "+result[0].department_name+" department\n"; 
+			console.log("item EXIST!!!");
+			next();
+			let q = `UPDATE ${tableName}
+			SET stock_quantity=${result[0].stock_quantity}-${req.body.quantity}
+			WHERE item_id=${result[0].item_id}`
+			con.query(q,(err,result)=>{
+				if(err) throw err;
+			})
+		};
+		res.render("buynow",{
+			response: response
+		});
+
+	})
 });
 //////////////////////////DATABASE-CLASS////////////////////////////////////////////////
 // database class
@@ -72,9 +154,11 @@ class BAMAZON
 		this.units = units;
 
 	}
-	createTable(con) {
+	createTable(con) 
+	{
 		const db = 'SHOW DATABASES';
-		con.query(db, (err, result) => {
+		con.query(db, (err, result) => 
+		{
 			if (err) throw err;
 			console.log('\n---------------databases---------------');
 			console.log(result);
@@ -104,15 +188,15 @@ class BAMAZON
 
 		var sqlV = `INSERT INTO ${this.table}(prod_name, department_name, price, stock_quantity) VALUES?`;
 		var values = [
-			['cheese', 'food', '1.35', '10'],
+			['stake', 'food', '1.35', '10'],
 			['steve maddan shoes item 221', 'shoes', '234.78', '2'],
-			['burger', 'food', '3.43', '19'],
+			['burger and fries', 'food', '3.43', '19'],
       ['nba 2k18', 'games', '140', '50'],
-      ['meat', 'food', '15.35', '11'],
+      ['mutton', 'food', '15.35', '11'],
 			['clippers', 'accessories', '24.78', '28'],
 			['flashdrive', 'electronics', '30.43', '119'],
       ['battery', 'accessories', '3.99', '52'],
-      ['soda', 'beverage', '1.43', '100'],
+      ['coke', 'beverage', '1.43', '100'],
 			['pillow', 'housing', '32.99', '22'],
 		];
 		con.query(sqlV, [values], (err, result) => {
@@ -133,7 +217,11 @@ class BAMAZON
 }
 
 //the connection is slow enough that this wil create first
-user1 = new BAMAZON('test');
+var table1 = "test";
+var table2 = "test2";
+var tableName = table1;
+user1 = new BAMAZON(table1);
+user2 = new BAMAZON(table2);
 
 var PORT = process.env.PORT || 8080;
 app.listen(PORT,()=>{
