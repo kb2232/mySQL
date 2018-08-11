3caf258576b491e8c858c3611bcc82eb9b645846
kunle babatunde <kb2232@nyu.edu>
2018-08-11 11:26:27 -0400

    seciton 10

---
+++ section10.sql
@@ -0,0 +1,41 @@
+-- -->CHAR(n): it has a fixed length - every input would have n length no more!!!
+-- //examples are: state abbreviations (NY, NJ, etc..), gender, Y/N
+-- -->VARCHAR(n): variable length;
+
+-- -->NUMBERS
+-- 1. INT : works with whole numbers;
+
+-- 2. DECIMAL(n, m): (precision is awesome)
+-- n - total number of digits
+-- m - total number after digits;
+
+-- 3. FLOAT and DOUBLE - store larger numbers using less space
+-- - it comes in the cost of precision
+-- float : after 7 digits precision is lost (4 bytes)
+-- double : after 15 digits precision is lost (8 bytes);
+
+-- 1. DATE: format is 'YYYY-MM-DD'
+-- 2. TIME: format is 'HH:MM:SS'
+-- 3. DATETIME: format is 'YYYY-MM-DD HH:MM:SS';
+SHOW DATABASES;
+USE bamazon;
+SHOW TABLES;
+
+CREATE TABLE people (
+  name VARCHAR(100),
+  birthDATE DATE,
+  birthTIME TIME,
+  birthDAY DATETIME
+);
+INSERT INTO people 
+(name, birthdate, birthtime, birthday)
+VALUES 
+('Padma','1983-11-11','10:07:35','1983-11-11 10:07:35'),
+('Larry','1943-12-25','04:10:42','1943-12-25 04:10:42');
+
+SELECT * FROM people;
+
+INSERT INTO people 
+(name, birthdate, birthtime, birthday)
+VALUES 
+('Microwave',CURDATE(), CURTIME(), NOW());
